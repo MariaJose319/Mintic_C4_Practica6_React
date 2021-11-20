a4adebc1970c77c7676450e382e41d9f7398b5f2
MariaJose319 <mjsierra319@gmail.com>
2021-11-20 10:24:06 -0500

    Primer Commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+function NavBar() {
+    return (
+        /* la palabra class cambia por className */
+        <nav className="navbar navbar-expand-lg navbar-light" >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="none">MISION TIC</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="none">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="none">Noticias</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <a className="btn btn-outline-success" href="login.html">Login</a>
+            </form>
+          </div>
+        </div>
+      </nav>
+    );
+}
+
+/* no olvidar el export */
+export default NavBar;
